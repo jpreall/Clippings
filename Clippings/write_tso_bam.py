0e0c0e9903bbabbce4ce51323b62d8510e5d7ac4
Brian He <bh.brianhe@gmail.com>
2022-03-15 14:57:11 -0400

    Allow user to specify outdir name. Test functionality.

--- Clippings/write_tso_bam.py
+++ Clippings/write_tso_bam.py
@@ -36,9 +36,9 @@ class TSOReadCounter(ParaReadProcessor):
         reads = self.fetch_chunk(chromosome)
 
         print('Time started for TSO BAM ({0}) write-out:'.format(chromosome), time.asctime())
-        tso_bamFolder = 'tso_bamFolder'
-        tso_bamFolderPath = os.path.join(os.getcwd(), tso_bamFolder)
-
+        # tso_bamFolder = 'tso_bamFolder'
+        # tso_bamFolderPath = os.path.join(os.getcwd(), tso_bamFolder)
+        tso_bamFolderPath = os.getcwd()
         TSOBAM_FILENAME = os.path.join(tso_bamFolderPath, str(chromosome) + '_tmp_tso.bam')
 
         TSOBAM = pysam.AlignmentFile(TSOBAM_FILENAME, "wb", template=get_template(args.BAMFILE))
@@ -49,14 +49,15 @@ class TSOReadCounter(ParaReadProcessor):
                 tso_reads += 1
         TSOBAM.close()
 
-        #_LOGGER.debug("Chromosome: '{}'; n_reads {}".format(chromosome, n_reads))
+        # _LOGGER.debug("Chromosome: '{}'; n_reads {}".format(chromosome, n_reads))
         with open(self._tempf(chromosome), 'w') as f:
             f.write("{}\t{}".format(chromosome, tso_reads))
         return chromosome
 
 
 def get_template(full_path_to_BAM):
-    BAM = os.path.basename(full_path_to_BAM)
+    # BAM = os.path.basename(full_path_to_BAM)
+    BAM = full_path_to_BAM
     alignments_template = pysam.AlignmentFile(BAM, "rb")
     return alignments_template
 
@@ -65,32 +66,17 @@ def main(args):
     import time
     print('****MAIN STEP****: START', time.asctime())
 
-    tso_bamFolder = 'tso_bamFolder'
+    tso_bamFolder = args.out
     import os
     tso_bamFolderPath = os.path.join(os.getcwd(), tso_bamFolder)
     os.mkdir(tso_bamFolderPath)
+    os.chdir(tso_bamFolderPath)
     print('tso_bamFolderPath', tso_bamFolderPath)
 
     # custom outpath currently doesnt work
-    outpath = args.out  # NEED A FRIENDLY WAY TO CHECK IF PATH EXISTS
+    # outpath = args.out  # NEED A FRIENDLY WAY TO CHECK IF PATH EXISTS
     BAM = args.BAMFILE
     BAM_align = pysam.AlignmentFile(BAM, 'rb')
-
-    # if outpath == None:
-    #    outpath = os.path.dirname(full_path_to_BAM)
-
-    # if not os.path.isdir(outpath):
-    #    print('Warning: output path not found')
-
-    #OUTBAM_FILENAME = os.path.join(outpath, 'TSO_reads.bam')
-    #TMPBAM_FILENAME = os.path.join(outpath, 'tmp_tso.bam')
-
-    #write_degraded_bam(full_path_to_BAM, TMPBAM_FILENAME, OUTBAM_FILENAME)
-
-    #jsonFolder = 'jsonFolder'
-    #jsonPath = os.path.join(os.getcwd(), jsonFolder)
-    # os.mkdir(jsonPath)
-    #print('jsonPath: ', jsonPath)
 
     print("Creating counter")
     counter = TSOReadCounter(args.BAMFILE, cores=args.cores, action="CountReads")
@@ -104,7 +90,7 @@ def main(args):
     good_chromosomes = counter.run()
     print('****MAIN STEP****: FINISHED counter.run()', time.asctime())
 
-    #print("Collecting read counts: {}".format(args.outdictionary))
+    # print("Collecting read counts: {}".format(args.outdictionary))
     counter.combine(good_chromosomes, chrom_sep="\n")
     print('****MAIN STEP****: FINISHED COMBINING', time.asctime())
 
@@ -113,20 +99,20 @@ def main(args):
     print("Concatenating bam files", time.asctime())
     # Now that the chromosome bams are written out, we can proceed
     allBam = '*.bam'
-    bamFiles = glob.glob(os.path.join(tso_bamFolder, allBam))
-    #print('bamFiles: ', bamFiles)
+    bamFiles = glob.glob(os.path.join(os.getcwd(), allBam))
+    # print('bamFiles: ', bamFiles)
 
     # Get the ordered list of chromosomes based on original bam
     chr_list_order = list()
     for chromosome in BAM_align.header['SQ']:
-        #print(chromosome['SN'] + '_tmp_tso.bam')
+        # print(chromosome['SN'] + '_tmp_tso.bam')
         chr_list_order.append(chromosome['SN'] + '_tmp_tso.bam')
 
     # next get the full path for each chromosome
     chr_path_list_order = list()
     for i in chr_list_order:
-        print(os.path.join(tso_bamFolder, i))
-        chr_path_list_order.append(os.path.join(tso_bamFolder, i))
+       # print(os.path.join(os.getcwd(), i))
+        chr_path_list_order.append(os.path.join(os.getcwd(), i))
 
     # then get the subset of chromosome bams that exist
     # in the folder from chr_path_list_order
@@ -144,20 +130,22 @@ def main(args):
         print('Generating BAM index... ', time.asctime())
         pysam.index(full_tso_bam)
         print('Output file:', full_tso_bam)
-        print('File size = ', np.round(os.path.getsize(full_tso_bam) / 1024**2, 2), 'MB')
-
+        print('File size = ', np.round(os.path.getsize(full_tso_bam) / 1024 ** 2, 2), 'MB')
+    else:
+        print('Path doesn\'t exist')
+    print('Finished script', time.asctime())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('BAMFILE', help='Input bam file')
-    # parser.add_argument('--out', dest='out', help='Output folder', default=None)
-    parser.add_argument('-C', '--cores', default=10, help="Number of cores.")
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit()
-    args = parser.parse_args()
+    parser.add_argument('--outfolder', dest='out', help='Output folder', default='tso_bamFolder')
+    parser.add_argument('--cores', default=10, help="Number of cores.")
+    # testing purposes start
+#    sys.argv = ['write_tso_bam.py',
+#                '/mnt/grid/scc/data/Preall/Preall_CR01/count/Preall_CR01_H_neg/outs/possorted_genome_bam.bam',
+#                '--outfolder', 'tso_bamFolder_CR01_H_neg',
+#                '--cores', '10']
+#    print(sys.argv[1:])
+    # testing purposes end
+    args = parser.parse_args(sys.argv[1:])
     main(args)
-
-# if __name__ == "__main__":
-#    main(sys.argv[1:])

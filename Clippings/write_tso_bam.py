f5fda1b16cdee6094496c769ac523b814f73a68f
Jonathan Preall <jpreall@cshl.edu>
2022-10-01 00:50:40 -0400

    Added back simple parallel processing with write_tso_bam.py

--- Clippings/write_tso_bam.py
+++ Clippings/write_tso_bam.py
@@ -18,51 +18,90 @@ import argparse
 import pysam
 import numpy as np
 
-def get_template(full_path_to_BAM):
-    BAM = full_path_to_BAM
-    alignments_template = pysam.AlignmentFile(BAM, "rb")
-    return alignments_template
-
-def write_degraded_bam_simple(BAM, OUTBAM_FILENAME):
+def write_degraded_bam_multi(
+    BAM, 
+    OUTBAM_FILENAME, 
+    threads: int = 6):
     '''
     Writes a subsetted BAM file containing only reads with the ts:i tag
-	There is probably a good way to parallelize this in the future
-	'''
-    
-    sys.stdout.write(f'Extracting TSO-containing reads from {BAM} ... \n')
-    alignments = pysam.AlignmentFile(BAM, "rb")
-    OUTBAM = pysam.AlignmentFile(OUTBAM_FILENAME, "wb", template=alignments)
-    
-    tally = 0
-    
-    for read in alignments.fetch(until_eof=True):
-        tally += 1
-        if tally % 1e7 == 0:
-            sys.stdout.write(f'Lines read: {tally:,} \n')
-			
-        if read.has_tag("ts:i"):
-            OUTBAM.write(read)
+    '''
+    from multiprocessing import Process
+    from multiprocessing import cpu_count
 
-    OUTBAM.close()
-    alignments.close()
-
+    #print(threads, type(threads))
+    threads = int(threads)
+    num_cores = int(cpu_count())
+    if threads > num_cores:
+        threads = num_cores-1
+    sys.stdout.write(f'Using {threads} threads. \n')
+    
     if os.path.exists(OUTBAM_FILENAME):
-        sys.stdout.write('Generating BAM index... \n')
+        BAD_OUTBAM_FILENAME = OUTBAM_FILENAME
+        OUTBAM_FILENAME = OUTBAM_FILENAME.split('.bam')[0] + '-1.bam'
+        sys.stderr.write(f'Output BAM {BAD_OUTBAM_FILENAME} exists. using {OUTBAM_FILENAME} \n')
+        
+    def filesize(file):
+        return np.round(os.path.getsize(file) / 1024**2,2)
+    
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+    
+    def fetchTSOs(chrom,BAM):
+        bam = pysam.AlignmentFile(BAM,'rb')
+        TMPBAMFILE = os.path.realpath(str(chrom) + "_tmp.bam")
+        TMPBAM = pysam.AlignmentFile(TMPBAMFILE, "wb", template=bam)
+        
+        Itr = bam.fetch(
+            chrom,
+            multiple_iterators=True, 
+            until_eof=True,
+        )
+        
+        for read in Itr:
+            if read.has_tag("ts:i"):
+                TMPBAM.write(read)                
+        TMPBAM.close()
+        # Write a logging function soon
+        #sys.stderr.write(f'{os.path.basename(TMPBAMFILE)}: {filesize(TMPBAMFILE)} MB \n')
+        
+    alignments = pysam.AlignmentFile(BAM, "rb")
+    chroms = alignments.header.references
+    
+    jobs = [Process(target=fetchTSOs(chrom, BAM)) for chrom in chroms]
+    for i in chunks(jobs,threads):
+        for j in i:
+            j.start()
+
+    tmpfiles = [str(chrom) + "_tmp.bam" for chrom in chroms]
+    pysam.cat('-o', OUTBAM_FILENAME, *tmpfiles)
+    
+    for t in tmpfiles:
+        os.remove(t)
+    
+    # Write BAM index
+    if os.path.exists(OUTBAM_FILENAME):
+        sys.stdout.write(f'Generating BAM index... for {OUTBAM_FILENAME} \n')
         pysam.index(OUTBAM_FILENAME)
         sys.stdout.write(f'Output file: {OUTBAM_FILENAME} \n')
-        file_size = np.round(os.path.getsize(OUTBAM_FILENAME) / 1024**2,2)
-        sys.stdout.write(f'File size = {file_size} MB \n')
-
-    OUTBAM.close()
+        file_size = filesize(OUTBAM_FILENAME)
+        sys.stdout.write(f'File size = {file_size} MB\n')
+        
+    alignments.close()
 
 def main(args):
     outfile = args.outfile
     BAM = args.BAMFILE
-    write_degraded_bam_simple(BAM, outfile)
+    threads = args.threads
+    write_degraded_bam_multi(BAM, outfile, threads=threads)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('BAMFILE', help='Input bam file')
-    parser.add_argument('--o', dest='outfile', help='Output filename', default='TSO_reads.bam')
+    parser.add_argument('BAMFILE', \
+        help='Input bam file')
+    parser.add_argument('--o', dest='outfile', \
+        help='Output filename', default='TSO_reads.bam')
+    parser.add_argument('--threads', dest='threads', \
+        help='Number of CPU threads to use to speed up processing. Default=4', default=4)
     args = parser.parse_args(sys.argv[1:])
     main(args)
